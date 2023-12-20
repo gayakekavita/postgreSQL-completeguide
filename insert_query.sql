5f50f4c0daa6482a18cab5dc912c33d8e9e1472f
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-12-20 14:24:48 +0530

    insert_query.sql

--- insert_query.sql
+++ insert_query.sql
@@ -18,3 +18,11 @@ INSERT INTO students(name,address,roll_no,marks)VALUES('kavitri','pune',20, 90.8
                      90.80
     INSERT,INTO and VALUES are the keywords.
 */
+
+INSERT INTO students(name,address,roll_no,marks)VALUES('kavi','panvel',30, 90),('kaya','mumbai',45,89.98),('jenny','us',56,88.98);
+
+
+?
+/*
+  inserting multiple records at a time  
+*/
