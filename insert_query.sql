7572520f50352b9f016c29c6a3bcfa4ac2a7ff9e
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-12-20 14:18:06 +0530

    Create insert_query.sql

---
+++ insert_query.sql
@@ -0,0 +1,20 @@
+INSERT INTO students(name,address,roll_no,marks)VALUES('kavitri','pune',20, 90.80);
+/*Above query is used to insert values into the table
+
+  Syntax for insert query is:
+      INSERT INTO table_name('column_1','column_2',.......,'column_n') VALUES (value1,value2,.....value_n);
+
+  here we inserting data into table :
+                             students 
+        columns are : 
+                    name
+                    address
+                    roll_no
+                    marks
+        values are :
+                    "kavitri"
+                    "pune"
+                     20
+                     90.80
+    INSERT,INTO and VALUES are the keywords.
+*/
