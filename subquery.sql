1274f7153a5f7cac4b4938329afaf40936a854ca
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-01-01 09:35:53 +0530

    Update subquery.sql

--- subquery.sql
+++ subquery.sql
@@ -1,1 +1,14 @@
 select name,price FROM products where price> (select MAX(price) from products where department='Home');
+select  name, price/weight AS ratio from products;
+
+select name,ratio FROM(select  name, price/weight AS ratio from products)AS p where ratio>5;
+
+Return value 
+SELECT *FROM(SELECT MAX(price) from products) AS p;--subquery 
+or
+SELECT MAX(price) from products;--simple query
+
+
+
+SELECT AVG(order_count) FROM(SELECT user_id ,COUNT(*) AS order_count FROM orders GROUP BY user_id)AS p;
+SELECT AVG(p.order_count) FROM(SELECT user_id ,COUNT(*) AS order_count FROM orders GROUP BY user_id)AS p;
