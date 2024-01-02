5073693782e92a27a1145ecba2be905746feec4b
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-01-02 16:24:12 +0530

    Update subquery.sql

--- subquery.sql
+++ subquery.sql
@@ -12,3 +12,12 @@ SELECT MAX(price) from products;--simple query
 
 SELECT AVG(order_count) FROM(SELECT user_id ,COUNT(*) AS order_count FROM orders GROUP BY user_id)AS p;
 SELECT AVG(p.order_count) FROM(SELECT user_id ,COUNT(*) AS order_count FROM orders GROUP BY user_id)AS p;
+
+
+select name,department FROM products where department NOT IN(select department FROM products where price<100);
+select name,price FROM products where price> ALL(select price FROM products where department='Industrial');
+select name,price FROM products where price> ALL(select price FROM products where department='Industrial');
+select name,price FROM products where price> SOME(select price FROM products where department='Industrial');
+select name,price,department FROM products where price> SOME(select price FROM products where department='Industrial');
+select name,price, department FROM products as P1 where price=(select MAX(price) FROM products AS p2 where p2.department=P1.department);
+select p1.name ,(select COUNT(*)from orders as o1 where o1.product_id=p1.id)AS num_of_orders   FROM products as p1;
