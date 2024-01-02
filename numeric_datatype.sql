138e6ed1eb3906fd5ec58ff1beffdfa02c6d72d7
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-01-02 17:40:55 +0530

    Create numeric_datatype.sql

---
+++ numeric_datatype.sql
@@ -0,0 +1,31 @@
+--Three categoris of numeric data types are 
+ /*    1.Decimal numbers
+     2.Numbers without decimal point
+     3.No decimal and auto increment
+       
+1.Numbers without decimal point:
+     1.1: Integer
+     1.2:smallint
+     1.3:bigint
+       
+2.No decimal and auto increment:
+    2.1:serial
+    2.2:smallserial
+    2.3:bigserial
+
+ 3.Decimal numbers:
+    3.1:decimal
+    3.2:numeric
+    3.3:float
+    3.4:real
+    3.5:double precision
+
+*/
+
+select 2+2;
+--ans =4
+select (2);
+--ans =2
+select(2.0:: Integer)
+--ans=2 beacuse 2.0 is converted into integer value.
+
