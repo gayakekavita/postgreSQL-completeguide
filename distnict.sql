a8724ef44683b1cb85b839a0647d2063c49c48bd
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-01-02 16:38:43 +0530

    Update distnict.sql

--- distnict.sql
+++ distnict.sql
@@ -1,3 +1,14 @@
+Inside a table, a column often contains many duplicate values; and sometimes you only want to list the different (distinct) values.
+  Syntax
+SELECT DISTINCT column1, column2, ...
+FROM table_name;
+
+
+The distinct keyword is used in conjunction with the select keyword.
+  It is helpful when there is a need to avoid duplicate values present in any specific columns/table. 
+  When we use distinct keywords only the unique values are fetched. 
+  If a distinct keyword is used with multiple columns, the distinct combination is displayed in the result set.
+
 select DISTINCT name FROM products;
 select COUNT(DISTINCT name) FROM products;
 select COUNT(DISTINCT department) FROM products;
