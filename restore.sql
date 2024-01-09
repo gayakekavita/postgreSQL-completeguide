e6e18536c8e3cb99f5f0c3994bf259f74b801fb2
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2024-01-09 20:43:57 +0530

    Create restore.sql

---
+++ restore.sql
@@ -0,0 +1,10 @@
+1.Click on database.
+2. Click on Restore , new window will open.
+3. Click on file icon ,new window will open.
+4.Select file.
+5. Make on: 
+Disable options : triggers 
+Query options: Single transaction
+Data options:
+     Types of data: Only data
+     Do not save: Owner 
